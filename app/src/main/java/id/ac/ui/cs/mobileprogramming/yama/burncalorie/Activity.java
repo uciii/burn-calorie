5fc4be3177d30cac986b651f9178b11e65f208e8
uciii <sef.gustingurah.yama@gmail.com>
2020-11-03 10:02:07 +0700

    make activity class

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/yama/burncalorie/Activity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/yama/burncalorie/Activity.java
@@ -1,4 +1,45 @@
 package id.ac.ui.cs.mobileprogramming.yama.burncalorie;
 
 public class Activity {
+    String title;
+    String info;
+    String description;
+    String type;
+    String url;
+    Integer calorie;
+
+    public Activity(String title, String description, String type, String url, Integer calorie){
+        this.title = title;
+        this.description = description;
+        this.type = type;
+        this.url = url;
+        this.calorie = calorie;
+
+        if(description.length()<150) this.info = description + "...";
+        else this.info = description.substring(0,150) + "...";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getCalorie() {
+        return calorie;
+    }
 }
