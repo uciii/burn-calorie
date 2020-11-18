324c2e8fc1c90bd5352c6c9bc534ceae7c227556
uciii <sef.gustingurah.yama@gmail.com>
2020-11-18 21:29:15 +0700

    edit service to music

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/yama/burncalorie/MusicService.java
@@ -0,0 +1,42 @@
+package id.ac.ui.cs.mobileprogramming.yama.burncalorie;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import java.util.concurrent.TimeUnit;
+
+public class MusicService extends Service {
+    private MediaPlayer player;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //getting systems default ringtone
+        player = MediaPlayer.create(this, R.raw.bensound);
+        //setting loop play to true
+        //this will make the ringtone continuously playing
+        player.setLooping(true);
+
+        //staring the player
+        player.start();
+
+        //we have some options for service
+        //start sticky means service will be explicity started and stopped
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+    }
+}
