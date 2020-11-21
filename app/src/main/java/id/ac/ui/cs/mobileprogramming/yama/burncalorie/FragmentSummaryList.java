d3bf8e7152126395d99aee98672ba06c8b47ddb9
uciii <sef.gustingurah.yama@gmail.com>
2020-11-21 10:48:43 +0700

    add content provider for store and share screenshot

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/yama/burncalorie/FragmentSummaryList.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/yama/burncalorie/FragmentSummaryList.java
@@ -1,6 +1,10 @@
 package id.ac.ui.cs.mobileprogramming.yama.burncalorie;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +13,15 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class FragmentSummaryList extends Fragment {
@@ -79,6 +87,9 @@ public class FragmentSummaryList extends Fragment {
                     .commit();
         });
         share = v.findViewById(R.id.share);
+        share.setOnClickListener(f2->{
+            shareButton();
+        });
         recyclerView = v.findViewById(R.id.recycler_view2);
 
         linearLayoutManager = new LinearLayoutManager(getActivity());
@@ -86,5 +97,40 @@ public class FragmentSummaryList extends Fragment {
         recyclerView.setAdapter(mainAdapter);
 
         return v;
+    }
+
+    public void shareButton() {
+        View view1 = getActivity().getWindow().getDecorView().getRootView();
+        view1.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(view1.getDrawingCache());
+        view1.setDrawingCacheEnabled(false);
+
+        String filePath = Environment.getExternalStorageDirectory() + "/Download/" +
+                Calendar.getInstance().getTime().toString() + ".jpg";
+        File fileScreenshot = new File(filePath);
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(fileScreenshot);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        onShareOnePhoto(fileScreenshot);
+    }
+
+    private void onShareOnePhoto(File file) {
+        Uri path = FileProvider.getUriForFile(getActivity(),
+                "id.ac.ui.cs.mobileprogramming.yama.burncalorie", file);
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "My Current Activity");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, path);
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        shareIntent.setType("image/*");
+        startActivity(Intent.createChooser(shareIntent, "Share..."));
     }
 }
